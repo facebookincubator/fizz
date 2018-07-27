d97742a5325d8b643129e6b83cffed032f88ab2b
Alex Guzman <reanimus@fb.com>
2018-07-27 15:01:21 -0700

    Add license header to fbcode builder config
    Summary: As it says on tin
    
    Reviewed By: siyengar
    
    Differential Revision: D9034827
    
    fbshipit-source-id: 0b3c549b92c8d6dee016b2fc7ad568fc2dbaf146

--- build/fbcode_builder_config.py
+++ build/fbcode_builder_config.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python
+
+# Copyright (c) 2018-present, Facebook, Inc.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
