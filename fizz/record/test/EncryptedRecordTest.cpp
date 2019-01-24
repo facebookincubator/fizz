cac6c6b817f8eae21d100bffdbb69a69898c65e0
Subodh Iyengar <subodh@fb.com>
2019-01-24 02:00:04 -0800

    add test that buffer is not shared
    Summary:
    Add test to make sure encrypted record layer
    does not pass in a shared iobuf to the aead
    
    Reviewed By: knekritz
    
    Differential Revision: D13725916
    
    fbshipit-source-id: 4c2de76392002dc4ed2a69fe9be2691789e9644a

--- fizz/record/test/EncryptedRecordTest.cpp
+++ fizz/record/test/EncryptedRecordTest.cpp
@@ -93,6 +93,7 @@ TEST_F(EncryptedRecordTest, TestReadAppData) {
   addToQueue("17030100050123456789");
   EXPECT_CALL(*readAead_, _decrypt(_, _, 0))
       .WillOnce(Invoke([](std::unique_ptr<IOBuf>& buf, const IOBuf*, uint64_t) {
+        EXPECT_FALSE(buf->isShared());
         expectSame(buf, "0123456789");
         return getBuf("1234abcd17");
       }));
