60ef8405a44d547955b3ae67178590a90a8cb122
Zhiyi Zhang <zhiyi@fb.com>
2020-07-15 10:58:14 -0700

    Add fizz client LoadGen tool and fizz server benchmark tool
    Summary:
    Add client and server benchmark tools into fizz command line tools
    * Server supports a thread pool (with arg `-threads`) to improve the performance of handling TLS handshakes.
    * Client supports a thread pool (with arg `-threads`) to initiate multiple TLS handshakes in parallel. Total number of TLS handshakes can be configured with arg `-tasks`. If total # of tasks is larger than # of threads, tasks will be uniformly assigned to all the threads.
    * Client, using `-json` argument, after finishing all handshakes, will generate benchmark statistics, including average handshake latency, percentiles for all handshake latencies. A real example of `./fizz client_loadgen -cafile ~/fb-test/cert.pem -threads 10 -tasks 100 -json` is shown below.
    
    ```
    {
      "average_latency": 64,
      "precentile": {
        "25%": 41,
        "50%": 62,
        "75%": 84,
        "90%": 104
      },
      "total_tasks": 100,
      "total_threads": 10,
      "unit": "millisecond"
    }
    ```
    
    Reviewed By: mingtaoy
    
    Differential Revision: D22322694
    
    fbshipit-source-id: d67d5275a98421d4e8a7e73877f12358cce54631

--- fizz/tool/Commands.h
+++ fizz/tool/Commands.h
@@ -20,24 +20,33 @@ int fizzClientCommand(const std::vector<std::string>& args);
 int fizzServerCommand(const std::vector<std::string>& args);
 int fizzGenerateDelegatedCredentialCommand(
     const std::vector<std::string>& args);
+int fizzClientLoadGenCommand(const std::vector<std::string>& args);
+int fizzServerBenchmarkCommand(const std::vector<std::string>& args);
 const std::vector<std::string> utilityNames = {"client",
                                                "s_client",
                                                "server",
                                                "s_server",
-                                               "gendc"};
+                                               "gendc",
+                                               "client_loadgen",
+                                               "server_benchmark"};
 
 const std::map<std::string, std::function<int(const std::vector<std::string>&)>>
     fizzUtilities = {{"client", &fizzClientCommand},
                      {"s_client", &fizzClientCommand},
                      {"server", &fizzServerCommand},
                      {"s_server", &fizzServerCommand},
-                     {"gendc", &fizzGenerateDelegatedCredentialCommand}};
+                     {"gendc", &fizzGenerateDelegatedCredentialCommand},
+                     {"client_loadgen", &fizzClientLoadGenCommand},
+                     {"server_benchmark", &fizzServerBenchmarkCommand}};
 
 const std::map<std::string, std::string> utilityDescriptions = {
     {"client", "TLS 1.3 client"},
     {"s_client", "Alias for client"},
     {"server", "TLS 1.3 server"},
     {"s_server", "Alias for server"},
-    {"gendc", "Generate a delegated credential"}};
+    {"gendc", "Generate a delegated credential"},
+    {"client_loadgen",
+     "TLS 1.3 clients generating TLS handshakes for performance benchmark"},
+    {"server_benchmark", "TLS 1.3 servers for performance test"}};
 } // namespace tool
 } // namespace fizz
