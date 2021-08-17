fa2cd1e08f0de84750d1a9ed30c303ea4d4e236d
Zsolt Dollenstein <zsol@fb.com>
2021-08-17 01:59:35 -0700

    Enable BLACK by default
    Reviewed By: zertosh
    
    Differential Revision: D30343018
    
    fbshipit-source-id: a0464c91f2eb8dc5cff849e112efe50f879e7ef1

--- build/fbcode_builder_config.py
+++ build/fbcode_builder_config.py
@@ -10,41 +10,44 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
 
-'fbcode_builder steps to build & test Fizz'
+"fbcode_builder steps to build & test Fizz"
 
-import specs.gmock as gmock
 import specs.fmt as fmt
 import specs.folly as folly
+import specs.gmock as gmock
 import specs.sodium as sodium
-
 from shell_quoting import ShellQuoted
 
 
 def fbcode_builder_spec(builder):
     builder.add_option(
-        'fizz/_build:cmake_defines',
+        "fizz/_build:cmake_defines",
         {
-            'BUILD_SHARED_LIBS': 'OFF',
-            'BUILD_TESTS': 'ON',
-        }
+            "BUILD_SHARED_LIBS": "OFF",
+            "BUILD_TESTS": "ON",
+        },
     )
     return {
-        'depends_on': [gmock, fmt, folly, sodium],
-        'steps': [
-            builder.fb_github_cmake_install('fizz/_build', '../fizz', 'facebookincubator'),
-            builder.step(
-                'Run fizz tests', [
-                    builder.run(
-                        ShellQuoted('ctest --output-on-failure -j {n}')
-                        .format(n=builder.option('make_parallelism'), )
-                    )
-                ]
+        "depends_on": [gmock, fmt, folly, sodium],
+        "steps": [
+            builder.fb_github_cmake_install(
+                "fizz/_build", "../fizz", "facebookincubator"
             ),
-        ]
+            builder.step(
+                "Run fizz tests",
+                [
+                    builder.run(
+                        ShellQuoted("ctest --output-on-failure -j {n}").format(
+                            n=builder.option("make_parallelism"),
+                        )
+                    )
+                ],
+            ),
+        ],
     }
 
 
 config = {
-    'github_project': 'facebookincubator/fizz',
-    'fbcode_builder_spec': fbcode_builder_spec,
+    "github_project": "facebookincubator/fizz",
+    "fbcode_builder_spec": fbcode_builder_spec,
 }
