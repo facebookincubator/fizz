8c599fc7564a3f906b19ef0a49c1e89114b3abe9
Mingtao Yang <mingtao@fb.com>
2021-10-15 10:50:18 -0700

    AsyncKTLSSocket read path
    Summary:
    Adds AsyncKTLSSocket, a separate AsyncSocket type that is intended to represent
    a connection with kTLS enabled.
    
    After the initial setup in the constructor, AsyncKTLSSocket behaves pretty
    much identically to AsyncSocket. The main logic that is needed is to properly
    handle non-application data that might be sent by the peer.
    
    AsyncKTLSSocket overrides `performRead` to utilize the more complete `recvmsg(2)`
    API to retrieve content type information from the kernel. It handles a small
    subset of valid post-handshake messages and dispatches them to a
    constructor-supplied `TLSCallback` implementation.
    
    The overall idea is that when a Fizz handshake completes, we can extract a
    small subset of the Fizz connection state (namely just the key scheduler and
    a reference to the PskCache) to implement the `TLSCallback`.
    
    AsyncKTLSSocket reads all data, post-handshake or not, directly to the user
    supplied ReadCallback buffer. Since it is legal to invoke `getReadBuffer()`
    without invoking `readDataAvailable()`, we handle non-application data
    transparently. We will read the user data into the user supplied buffer and
    return READ_BLOCKING, which prevents the data that we read in the buffer from
    notifying the user.
    
    Reviewed By: reanimus
    
    Differential Revision: D22957599
    
    fbshipit-source-id: af747d9498173092d9c9dd9403515b32d8e605d2

---
+++ fizz/experimental/ktls/AsyncKTLSSocket.h
@@ -0,0 +1,117 @@
+/*
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+#include <fizz/experimental/ktls/KTLS.h>
+#include <folly/io/async/AsyncSocket.h>
+
+namespace fizz {
+
+/**
+ * AsyncKTLSSocket acts like an AsyncSocket, but utilizes kTLS to ensure that
+ * all data sent and received from it are encrypted and wrapped in TLS data
+ * records.
+ *
+ * This does not provide full functionality to TLS. AsyncKTLSSocket handles
+ * a subset of handshake messages to ensure interoperability with the majority
+ * of clients.
+ *
+ * Currently, this implementation of AsyncKTLSSocket:
+ *  * Only supports TLS 1.3
+ *  * Requires both TX and RX enabled at the same time.
+ *
+ * kTLS is made a separate type (rather than being implemented transparently
+ * in Fizz), because of the following reasons:
+ *  * The upgrade should be explicit. As part of the upgrade, we return
+ *    information on why the upgrade failed, so if the user is *expecting* to
+ *    use kTLS in the common case, the user will know how to remediate
+ *    situations like:
+ *        * tls module not being loaded (add tls to /etc/modules ...)
+ *        * cipher not supported (adjust server cipher preferences ...)
+ *  * Some of the original settings that the user specified for the original
+ *    Fizz socket will now be violated:
+ *      * e.g. record sizes. kTLS uses a hardcoded 16k max record size. This may
+ *        cause compatibility concerns
+ *  * We can throw away much of the state from AsyncFizzSocket and keep just
+ *    the bare minimum to handle TLS events. This allows us to save some
+ *    memory per socket.
+ */
+class AsyncKTLSSocket final : public folly::AsyncSocket {
+ public:
+  using UniquePtr =
+      std::unique_ptr<AsyncKTLSSocket, folly::DelayedDestruction::Destructor>;
+  struct TLSCallback {
+    virtual ~TLSCallback() = default;
+    /**
+     * receivedNewSessionTicket() is invoked whenever the peer sends a
+     * NewSessionTicket message.
+     *
+     * Implementations may choose to ignore this,
+     * but it is advised to update any associated PSK cache if the peer is
+     * issuing a new ticket to identify this session.
+     *
+     * NOTE: IOBufs point to borrowed memory valid for the lifetime of the
+     * invocation.  Implementations *must* first unshare() any buffers if
+     * they are needed past function return.
+     */
+    virtual void receivedNewSessionTicket(
+        AsyncKTLSSocket* /*sock*/,
+        fizz::NewSessionTicket /*ticket*/) {}
+  };
+
+  /**
+   * Constructs an AsyncKTLSSocket from an AsyncSocket that already has
+   * kTLS enabled.
+   *
+   * This could be the case if:
+   *  * You are taking over a detached file descriptor from another
+   * AsyncKTLSSocket
+   *  * You have established a ktls 1.3 connection out of band
+   */
+  AsyncKTLSSocket(
+      folly::EventBase* evb,
+      KTLSNetworkSocket fd,
+      std::unique_ptr<TLSCallback> tlsCallback)
+      : AsyncSocket(evb, fd), tlsCallback_(std::move(tlsCallback)) {}
+
+  bool hasBufferedHandshakeData() const {
+    return unparsedHandshakeData_ != nullptr;
+  }
+
+  std::string getSecurityProtocol() const override {
+    return "Fizz/KTLS";
+  }
+
+ private:
+  AsyncSocket::ReadResult
+  performRead(void** buf, size_t* buflen, size_t* offset) override;
+
+  AsyncSocket::ReadResult handleNonApplicationData(
+      uint8_t type,
+      folly::ByteRange payload);
+
+  AsyncSocket::ReadResult processAlert(folly::ByteRange payload);
+  AsyncSocket::ReadResult processHandshakeData(folly::ByteRange payload);
+
+  std::unique_ptr<TLSCallback> tlsCallback_;
+
+  // This is unlikely to be used (see
+  // [note.handshake_fits_in_record_assumption])
+  // so we opt for a heap allocation (IOBufQueue is 64 bytes as of this
+  // writing)
+  std::unique_ptr<folly::IOBufQueue> unparsedHandshakeData_;
+};
+} // namespace fizz
