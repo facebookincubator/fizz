401a47fe3ea6ec8019250aef12d5854bd056652c
John Reese <jreese@fb.com>
2022-05-15 12:53:03 -0700

    apply import merging for fbcode (5 of 11)
    Summary:
    Applies new import merging and sorting from µsort v1.0.
    
    When merging imports, µsort will make a best-effort to move associated
    comments to match merged elements, but there are known limitations due to
    the diynamic nature of Python and developer tooling. These changes should
    not produce any dangerous runtime changes, but may require touch-ups to
    satisfy linters and other tooling.
    
    Note that µsort uses case-insensitive, lexicographical sorting, which
    results in a different ordering compared to isort. This provides a more
    consistent sorting order, matching the case-insensitive order used when
    sorting import statements by module name, and ensures that "frog", "FROG",
    and "Frog" always sort next to each other.
    
    For details on µsort's sorting and merging semantics, see the user guide:
    https://usort.readthedocs.io/en/stable/guide.html#sorting
    
    Reviewed By: lisroach
    
    Differential Revision: D36402182
    
    fbshipit-source-id: 53d74e20bfdbeff46c91a6d9d2f2fbea4edaedbf

--- build/fbcode_builder_config.py
+++ build/fbcode_builder_config.py
@@ -5,10 +5,7 @@
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree.
 
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-from __future__ import unicode_literals
+from __future__ import absolute_import, division, print_function, unicode_literals
 
 "fbcode_builder steps to build & test Fizz"
 
