43f58fd3beda5be17714501e7e38db2539f88ea2
namanahuja <namanahuja@meta.com>
2024-06-06 16:14:07 -0700

    Update oss_buck_build_and_test.sh

--- oss_buck_build_and_test.sh
+++ oss_buck_build_and_test.sh
@@ -4,7 +4,7 @@
 
 if [ "$GITHUB_ACTIONS" == "true" ]; then
     TARGETS_FILE=$(mktemp)
-    buck2 targets //fizz/... | grep -F -v -f ./bad_targets | grep -v test>"$TARGETS_FILE"
+    ./buck2 targets //fizz/... | grep -F -v -f ./bad_targets | grep -v test>"$TARGETS_FILE"
     ./buck2 build @"$TARGETS_FILE"
 else
     dotslash-oss "$BUCK2" build //... && dotslash-oss "$BUCK2" test //...
