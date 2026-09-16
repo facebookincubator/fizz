f1a8055313092fa39f6ea1e204c47759812e77f0
Fred Qiu <qiu@meta.com>
2026-09-16 11:04:24 -0700

    Add copyright header for fizz source files
    Summary: Add copyright header for fizz source files.
    
    Reviewed By: knekritz
    
    Differential Revision: D120369352
    
    fbshipit-source-id: 306df2fe73c1a4671a4822f9ebcefbc4bd3e3dad

--- fizz/experimental/psp/PSP.cpp
+++ fizz/experimental/psp/PSP.cpp
@@ -1,3 +1,11 @@
+/*
+ *  Copyright (c), Meta Platforms, Inc. and affiliates
+ *  All rights reserved.
+ *
+ *  This source code is licensed under the BSD-style license found in the
+ *  LICENSE file in the root directory of this source tree.
+ */
+
 #include <fizz/experimental/psp/PSP.h>
 #include <fizz/util/Logging.h>
 #include <fmt/format.h>
